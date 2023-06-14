6e17354044e4b49d54cd12af2309e3b3aebee09e
Felipe Quiñonez <fdquinones@hotmail.com>
2023-06-14 21:03:38 +0000

    ajustes para trabajar con spotify

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
+import spotipy
 
-app = FastAPI()
+sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyClientCredentials(
+    client_id='926560c38be7457d82a4a191450d60b5',
+    client_secret='1540ff683c5640429701be0900ceba6f'
+))
+
+tags_metadata = [
+    {
+        "name": "users",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "personas",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "items",
+        "description": "Manage items. So _fancy_ they have their own docs.",
+        "externalDocs": {
+            "description": "Items external docs",
+            "url": "https://fastapi.tiangolo.com/",
+        },
+    },
+]
+
+app = FastAPI(openapi_tags=tags_metadata)
 
 class Persona (BaseModel):
     id: int
@@ -37,6 +62,11 @@ def eliminar_persona (persona_id: int):
     else:
         raise HTTPException(status_code=404, detail="Persona no encontrada")
 
+@app.get("/tracks/{track_id}")
+async def get_track(track_id: str):
+    track = sp.track(track_id)
+    return track
+    
 @app.get("/")
 def read_root():
     return {"Hello": "Interoperabilidad 8"}
